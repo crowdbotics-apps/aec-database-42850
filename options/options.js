5563e5d09943ee04975029f8b33e65b3823dada3
Crowdbotics <team@crowdbotics.com>
2023-08-15 00:53:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aec-database-42850",
+  url: "https://aec-database-42850.botics.co",
+  api: "https://aec-database-42850.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
